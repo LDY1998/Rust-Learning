2931a1b4cb82959dd563d008a275b6f8c73897c4
LDY1998 <caliudeyu2017@gmail.com>
2021-02-28 09:48:28 -0800

    bad_stack implement cons, car, cdr, map, filter

--- bad_stack/src/first.rs
+++ bad_stack/src/first.rs
@@ -8,11 +8,51 @@ pub enum List<T> {
     Elem(T, Box<List<T>>),
 }
 
-impl<T> List<T> {
+pub fn cons<T>(data: T, list: List<T>) -> List<T> {
 
-    pub fn insert(position: i8, data: T) {
+    List::Elem(data, Box::new(list))
+}
 
+pub fn car<T>(list: &List<T>) -> &T {
+    
+    match list {
+        &List::Empty => panic!("Can't call car on empty list"),
+        &List::Elem(ref data, _) => &data,
+    }
 
+}
+
+pub fn cdr<T>(list: &List<T>) -> &List<T> {
+
+    match list {
+        &List::Empty => panic!("Can't call cdr on empty list"),
+        &List::Elem(_, ref next) => &next
     }
 }
 
+pub fn map<T, F>(f: F,list: &List<T>) -> List<T> 
+where
+    F: Fn(&T) -> T
+{
+    match &list {
+        &List::Empty => List::Empty,
+        &List::Elem(ref data, next) => List::Elem(f(data), Box::new(map(f, &*next))), 
+    }
+}
+
+pub fn filter<T, F>(f: F, list: &List<T>) -> List<T>
+where 
+    F: Fn(&T) -> bool,
+    T: Clone,
+{
+    match &list {
+        &List::Empty => List::Empty,
+        &List::Elem(data, next) => {
+            if f(&data) {
+                List::Elem(data.clone(), Box::new(filter(f, &*next)))
+            } else {
+                filter(f, &*next)
+            }
+        }
+    }
+}
--- bad_stack/src/main.rs
+++ bad_stack/src/main.rs
@@ -1,4 +1,6 @@
-use first::List;
+use first::{List, cons, car, cdr, map, filter};
+use std::cmp::Ordering;
+
 
 mod first;
 
@@ -6,6 +8,29 @@ fn main() {
 
     let list: List<i32> = List::Elem(1, Box::new(List::Empty));
 
-    println!("{:?}", list);
-    
+    let li2: List<i32> = cons(1, cons(2, List::Empty));
+    println!("list1: {:?}", list);
+    println!("list2: {:?}", li2);
+
+    println!("Head of li2: {}", car(&li2));
+    println!("Second of li2: {:?}", cdr(&li2));
+
+    let li3 = map(|x| {
+        x+1
+    }, &li2);
+
+    println!("li2 after map: {:?}", li3);
+
+    println!("original li2: {:?}", li2);
+
+    let filtered_li2 = filter(|x| {
+        match x.cmp(&1) {
+            Ordering::Equal => true,
+            _ => false,
+        }
+    }, &li2);
+
+    println!("List after filtered: {:?}", filtered_li2);
+
+    println!("original li2: {:?}", li2);
 }
