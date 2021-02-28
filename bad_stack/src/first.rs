4e1125996d0e34513883f32afa5c521449da5fa2
LDY1998 <caliudeyu2017@gmail.com>
2021-02-27 22:12:23 -0800

    bad_stack trait skeleton

--- bad_stack/src/first.rs
+++ bad_stack/src/first.rs
@@ -7,3 +7,12 @@ pub enum List<T> {
     Empty,
     Elem(T, Box<List<T>>),
 }
+
+impl<T> List<T> {
+
+    pub fn insert(position: i8, data: T) {
+
+
+    }
+}
+
--- bad_stack/src/main.rs
+++ bad_stack/src/main.rs
@@ -3,7 +3,7 @@ use first::List;
 mod first;
 
 fn main() {
-   
+
     let list: List<i32> = List::Elem(1, Box::new(List::Empty));
 
     println!("{:?}", list);
