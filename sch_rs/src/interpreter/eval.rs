dc330a2583459ac097af6132859d821d13a3a93b
LDY1998 <caliudeyu2017@gmail.com>
2021-03-09 19:15:28 -0800

    basic value display and debug trait

--- sch_rs/src/interpreter/eval.rs
+++ sch_rs/src/interpreter/eval.rs
@@ -1,5 +1,8 @@
-use super::parser::Node;
+use fmt::Display;
 
+use super::parser::Node;
+use std::{cell::{RefCell}, collections::HashMap, rc::Rc};
+use std::fmt;
 
 
 
@@ -12,7 +15,60 @@ pub enum Value {
     List(Vec<Value>),
 }
 
-pub fn eval(nodes: &Vec<Node>) -> Result<Value, RuntimeError> {
+impl fmt::Display for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Symbol(s) => write!(f, "{}", s),
+            Value::Integer(i) => write!(f, "{}", i),
+            Value::List(values) => {
+                let strs: Vec<String> = values.iter().map(|v| {
+                    format!("{}", v)
+                }).collect();
 
-
+                write!(f, "({})", &strs.join(" "))
+            }
+        }
+    }
 }
+
+impl fmt::Debug for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Value::Symbol(s) => write!(f, "{}", s),
+            Value::Integer(i) => write!(f, "{}", i),
+            Value::List(values) => {
+                let strs: Vec<String> = values.iter().map(|v| {
+                    format!("{:?}", v)
+                }).collect();
+
+                write!(f, "({})", &strs.join(" "))
+            }
+            _ => write!(f, "{}", self)
+        }
+    }
+}
+
+pub struct Env {
+    parent: Option<Rc<RefCell<Env>>>,
+    values: HashMap<String , Value>,
+}
+
+impl Env {
+    fn new_child(env: Rc<RefCell<Env>>) -> Rc<RefCell<Env>> {
+        let new_env = Env {
+            parent: Some(env),
+            values: HashMap::new(),
+        };
+
+        Rc::new(RefCell::new(new_env))
+    }
+}
+
+
+/*
+   TODO: The public eval function to produce a value based on AST
+*/
+// pub fn eval(nodes: &Vec<Node>) -> Result<Value, RuntimeError> {
+
+
+// }
